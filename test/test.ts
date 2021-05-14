9e39f4b5e4bfdc0d296c4d137a3fcf033ccd5410
dr666m1 <skndr666m1@gmail.com>
2021-05-15 00:50:51 +0900

    improved error message in test

--- test/test.ts
+++ test/test.ts
@@ -1,10 +1,25 @@
+import { BigQuery } from "@google-cloud/bigquery";
 import printer from "../src/printer";
 import parser from "@dr666m1/bq2cst";
 
-it("parser", () => {
-  expect(parser.parse("SELECT 1")).toEqual(parser.parse("SELECT 1"));
-});
+async function query(sql: string) {
+  const client = new BigQuery();
+  const options = {
+    query: sql,
+  };
+  const [job] = await client.createQueryJob(options).catch((err) => {
+    console.log(`
+============= something when wrong while calling \`createQueryJob\` =============
+${err}
+Query: ${sql}
+=================================================================================`);
+    throw err;
+  });
+  const [rows] = await job.getQueryResults();
+  rows.forEach((row) => console.log(typeof row));
+}
 
-it("parser", () => {
-  expect(parser.parse("SELECT 1")).not.toEqual(parser.parse("select 1"));
+it("parser", async () => {
+  await query("select 1 as one union ll select 2");
+  expect(1).toEqual(1);
 });
