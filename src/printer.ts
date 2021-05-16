53486df578edb6205a4f3e2ddc26a57944bfb2cf
dr666m1 <skndr666m1@gmail.com>
2021-05-16 20:03:15 +0900

    now i can compile...

--- src/printer.ts
+++ src/printer.ts
@@ -4,13 +4,12 @@ import type { Doc, FastPath } from "prettier";
 
 const {
   builders: {
-    concat,
     //group,
-    hardline,
+    //hardline,
     //indent,
     //join,
     //line,
-    lineSuffix,
+    //lineSuffix,
     //lineSuffixBoundary,
     //literalline,
     //softline,
@@ -23,166 +22,105 @@ type Token = {
   literal: string;
 };
 
-type Node = {
+type RawNode = {
   token: Token | null;
-  children: { [key: string]: {Node: Node} | {NodeVec: Node[]} };
-  NodeType: string;
+  children: { [key: string]: { Node: RawNode } | { NodeVec: RawNode[] } };
+  node_type: string;
+  emptyLines?: number;
 };
+
+class Node {
+  private node: RawNode;
+  constructor(
+    private readonly path: FastPath,
+    private readonly printFunc: (path: FastPath) => Doc
+  ) {
+    /**
+     * NOTE
+     * you can assume type of `path.getValue()` to be `Node`
+     * because `Node[]` has been splitted in `printSQL`.
+     */
+    this.node = path.getValue();
+  }
+  print(key: string) {
+    if (key in this.node) {
+      return this.path.call((p) => p.call(this.printFunc, key), "children");
+    } else {
+      return "";
+    }
+  }
+}
 
 export const printSQL = (
   path: FastPath,
   _: Object, // options
   print: (path: FastPath) => Doc
-): Doc => {
-  // NOTE the type of `node` is any!
-  const node = path.getValue();
+): Doc | Doc[] => {
+  /**
+   * NOTE
+   * you can assume type of `path.getValue()` to be `Node | Node[]`.
+   * it is tested by `@dr666m1/bq2cst`
+   */
+  const node: RawNode | RawNode[] = path.getValue();
 
   if (Array.isArray(node)) {
-    for (let i = 0; i < node.length - 2; i++) {
+    for (let i = 0; i < node.length - 1; i++) {
       if ("semicolon" in node[i].children) {
         // end of statement
-        let endNode = node[i].children.semicolon.Node;
-        let endLine = endNode.token.line;
+        const semicolon = node[i].children.semicolon as { Node: RawNode };
+        let endNode = semicolon.Node;
+        const endToken = endNode.token;
+        if (endToken == null) {
+          throw new Error(`semicolon must have a \`token\` property but not found!`);
+        }
+        let endLine = endToken.line;
         if ("following_comments" in endNode.children) {
-          const len = endNode.children.following_comments.NodeVec.length;
-          endNode = endNode.children.following_comments.NodeVec[len - 1];
-          const endLiteral = endNode.token.literal;
+          const following_comments = endNode.children.following_comments as {
+            NodeVec: RawNode[];
+          };
+          const len = following_comments.NodeVec.length;
+          endNode = following_comments.NodeVec[len - 1];
+          const endLiteral = endToken.literal;
           let newLines = endLiteral.match(/\n/g);
           if (newLines) {
-            endLine = endNode.token.line + newLines.length;
+            endLine = endToken.line + newLines.length;
           }
         }
         // start of statement
         let startNode = node[i + 1];
-        while (
-          ["UNION", "INTERSECT", "EXCEPT"].indexOf(
-            startNode.token.literal.toUpperCase()
-          ) !== -1
-        ) {
-          if ("left" in startNode.children) {
-            startNode = startNode.children.left.Node;
-          } else if ("stmt" in startNode.children) {
-            startNode = startNode.children.stmt.Node;
-          } else {
-            return JSON.stringify(node);
-          }
+        while (startNode.node_type === "SetOperator") {
+          const left = startNode.children.left as { Node: RawNode };
+          startNode = left.Node;
         }
-        let startLine = startNode.token.line;
+        let startLine;
+        let startToken = startNode.token;
+        if (startToken) {
+          startLine = startToken.line;
+        } else {
+          // EOF
+          startLine = endLine + 1;
+        }
         if ("leading_comments" in startNode.children) {
-          startLine = startNode.children.leading_comments.NodeVec[0].token.line;
+          const leading_comments = startNode.children.leading_comments as {
+            NodeVec: RawNode[];
+          };
+          startToken = leading_comments.NodeVec[0].token;
+          if (startToken == null) {
+            throw new Error(`leading_comments must have a \`token\` property but not found!`);
+          }
+          startLine = startToken.line;
         }
-        node[i].children.emptyLines = startLine - endLine - 1;
+        node[i].emptyLines = startLine - endLine - 1;
       }
     }
-    return concat(path.map(print));
+    return path.map(print);
   }
   switch (node.node_type) {
     case "SelectStatement":
-      return printSelf(path, print);
+      return "SELECT";
+    case "EOF":
+      return "EOF";
     default:
-      return printSelf(path, print);
+      return "not implemented";
   }
-};
-
-const call = (
-  path: FastPath,
-  key: string,
-  print: (path: FastPath) => Doc
-): Doc => {
-  return path.call((p) => p.call(print, key), "children");
-};
-
-const printSelf = (
-  path: FastPath,
-  print: (path: FastPath) => Doc
-): Doc => {
-  const node = path.getValue();
-  // leading_comments
-  let leading_comments: Doc = "";
-  if ("leading_comments" in node) {
-    leading_comments = node.leading_comments.NodeVec.map(
-      (x: Node) => {
-        if (x.token == null) {
-          return [];
-        }
-        return [x.token.literal, hardline];
-      } // literallineWithoutBreakParent may be better
-    );
-  }
-  // self
-  let self;
-  if (node.token == null) {
-    self = "";
-  } else {
-    self = node.token.literal;
-  }
-  if (!node.notGlobal) {
-    if (
-      reservedKeywords.indexOf(self.toUpperCase()) !== -1 ||
-      // Field names are not allowed to start with the (case-insensitive) prefixes _PARTITION, _TABLE_, _FILE_ and _ROW_TIMESTAMP
-      self.match(/^_PARTITION/i) ||
-      self.match(/^_TABLE_/i) ||
-      self.match(/^_FILE_/i) ||
-      self.match(/^_ROW_TIMESTAMP/i)
-    ) {
-      self = self.toUpperCase();
-    }
-  }
-  // except
-  let except: Doc = "";
-  if ("except" in node) {
-    except = concat([" ", call(path, "except", print)]);
-  }
-  // replace
-  let replace: Doc = "";
-  if ("replace" in node) {
-    node.replace.Node.token.literal =
-      node.replace.Node.token.literal.toUpperCase();
-    replace = concat([" ", call(path, "replace", print)]);
-  }
-  // order
-  let order: Doc = "";
-  let null_order: Doc = "";
-  if ("order" in node) {
-    order = concat([" ", call(path, "order", print)]);
-  }
-  if ("null_order" in node) {
-    null_order = concat([
-      " ",
-      call(path, "null_order", print),
-    ]);
-  }
-  // comma
-  let comma: Doc = "";
-  if ("comma" in node) {
-    comma = call(path, "comma", print);
-  }
-  // following_comments
-  let following_comments: Doc = "";
-  if ("following_comments" in node) {
-    following_comments = lineSuffix(
-      node.following_comments.NodeVec.map((x: Node) => {
-        if (x.token == null) {
-          return [];
-        }
-        return concat([" ", x.token.literal]);
-      })
-    );
-  }
-  // alias
-  let alias: Doc[] = [""];
-  if ("as" in node) {
-    alias = [" ", call(path, "as", print)];
-  }
-  return concat([
-    leading_comments,
-    self,
-    except,
-    replace,
-    order,
-    null_order,
-    alias,
-    comma,
-    following_comments,
-  ]);
 };
