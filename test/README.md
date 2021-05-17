2243274ca40a6bab31f0578ac0263202b64c49da
dr666m1 <skndr666m1@gmail.com>
2021-05-18 00:38:34 +0900

    set timeout of test

---
+++ test/README.md
@@ -0,0 +1,14 @@
+## Prerequisite
+In order to execute tests, you have to setup authentication.
+The easiest way is using the Google Cloud SDK (your default GCP project will be used).
+
+```
+gcloud auth application-default login
+```
+
+## How to execute tests?
+In order to execute tests, all you have to do is run the following command.
+
+```
+npm test
+```
--- test/test.ts
+++ test/test.ts
@@ -1,7 +1,8 @@
 import { BigQuery } from "@google-cloud/bigquery";
-import parser from "@dr666m1/bq2cst";
-import printer from "../src/printer";
 import prettier from "prettier";
+
+jest.setTimeout(30 * 1000) // each test case must have shorter timeout than default
+const client = new BigQuery();
 
 const format = (sql: string) => {
   try {
@@ -10,34 +11,32 @@ const format = (sql: string) => {
       plugins: ["."],
     });
     return res;
-  } catch(err) {
+  } catch (err) {
     console.log(`
-======================= Error executing \`prettier.format\` =======================
+==================== Error executing \`prettier.format\` =======================
 ${err}
 Query: ${sql}
-===================================================================================`);
-    throw err
+================================================================================`);
+    throw err;
   }
 };
 
 const query = async (sql: string) => {
-  const client = new BigQuery();
   const options = {
     query: sql,
   };
   const [job] = await client.createQueryJob(options).catch((err) => {
     console.log(`
-======================= Error executing \`createQueryJob\` =======================
+==================== Error executing \`createQueryJob\` ========================
 ${err}
 Query: ${sql}
-==================================================================================`);
+================================================================================`);
     throw err;
   });
   await job.getQueryResults();
-}
+};
 
 const assertResultDoesNotChange = async (sql: string) => {
-  const client = new BigQuery();
   // original sql
   const options = {
     query: sql,
@@ -64,18 +63,40 @@ Formatted query: ${sql_after}
   const [rows_after] = await job_after.getQueryResults();
   expect(rows.length).toEqual(rows_after.length);
   rows.forEach((_, i) => expect(rows[i]).toEqual(rows_after[i]));
-}
+};
 
-beforeAll(
-  async () => {
-    await query(`DROP SCHEMA IF EXISTS prettier_plugin_bq_test CASCADE`);
-    await query(`CREATE SCHEMA prettier_plugin_bq_test`);
-    expect(1).toEqual(1);
-  },
-  10 * 1000
-);
-
-it("parser", async () => {
-  await assertResultDoesNotChange(`select 1;`);
+beforeAll(async () => {
+  await query(`DROP SCHEMA IF EXISTS prettier_plugin_bq_test CASCADE`);
+  await query(`CREATE SCHEMA prettier_plugin_bq_test`);
   expect(1).toEqual(1);
+});
+
+describe("SELECT statement", () => {
+  beforeAll(async () => {
+    await query(`
+CREATE TABLE prettier_plugin_bq_test.test_select AS
+SELECT  1 x,  1 y, 11 z UNION ALL
+SELECT  2,    1,   12   UNION ALL
+SELECT  3,    1,   13   UNION ALL
+SELECT  4,    1,   14   UNION ALL
+SELECT  5,    1,   15   UNION ALL
+SELECT  6,    1, NULL   UNION ALL
+SELECT  7,    1, NULL   UNION ALL
+SELECT  8,    1, NULL   UNION ALL
+SELECT  9,    1, NULL   UNION ALL
+SELECT 10,    1, NULL   UNION ALL
+SELECT 11,    2,   11   UNION ALL
+SELECT 12,    2,   12   UNION ALL
+SELECT 13,    2,   13   UNION ALL
+SELECT 14,    2,   14   UNION ALL
+SELECT 15,    2,   15   UNION ALL
+SELECT 16, NULL,   16   UNION ALL
+SELECT 17, NULL,   17   UNION ALL
+SELECT 18, NULL,   18   UNION ALL
+SELECT 19, NULL,   19   UNION ALL
+SELECT 20, NULL,   20`)
+  })
+  test("minimum", async () => {
+    await assertResultDoesNotChange(`select 1;`);
+  });
 });
