e3181cd7496f7201e237020abc508683d7ca24dc
dr666m1 <skndr666m1@gmail.com>
2023-04-16 19:05:02 +0900

    fix #166

--- expected_output/dml.sql
+++ expected_output/dml.sql
@@ -57,6 +57,16 @@ WHEN NOT MATCHED THEN INSERT ROW
 WHEN NOT MATCHED BY TARGET THEN INSERT (id, value) VALUES (1, 2)
 ;
 
+MERGE t1 AS t
+USING t2 AS s
+ON t.id = s.id
+WHEN NOT MATCHED BY TARGET THEN
+  -- break
+  INSERT (id, value)
+  -- break
+  VALUES (1, 2)
+;
+
 -- UPDATE
 MERGE dataset.t AS t
 USING dataset.s AS s
