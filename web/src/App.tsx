efbb20fcb606b2cda85b40872ac237fd313a41ce
kitta65 <26474260+kitta65@users.noreply.github.com>
2025-09-21 14:21:44 +0900

    impl ui

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,65 @@
+import clsx from "clsx";
+import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
+function prettify(sql: string) {
+  // TODO: exec prettier
+  return sql.toUpperCase();
+}
 
+function App() {
+  const [sql, setSql] = useState("select 1;");
   return (
     <>
-      <h1 className="font-extrabold text-5xl">Prettier Plugin BQ</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <header className={clsx("p-4", "font-extrabold text-5xl")}>
+        Prettier Plugin BQ
+      </header>
+      <textarea
+        id="sql"
+        className={clsx(
+          "shadow bg-white rounded-md",
+          // TODO: responsive width
+          "w-100 p-4 flex-grow m-4",
+          "outline-none outline-4 outline-dark focus:outline-solid",
+        )}
+        value={sql}
+        onChange={(e) => setSql(e.target.value)}
+      />
+      <div className="p-4">
+        {/* TODO: add options */}
+        <button
+          className={clsx(
+            "bg-dark text-background px-2 py-1 rounded-md font-bold",
+            "hover:bg-background hover:outline-4 hover:outline-dark hover:text-dark hover:-outline-offset-4",
+            "cursor-pointer shadow",
+          )}
+          onClick={() => {
+            setSql(prettify(sql));
+          }}
+        >
+          FORMAT
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <footer
+        className={clsx(
+          "p-4 bg-dark text-background w-full",
+          "flex flex-col items-center justify-center gap-y-1",
+        )}
+      >
+        <div>
+          <a
+            href="https://github.com/kitta65/prettier-plugin-bq"
+            className={clsx(
+              "cursor-pointer",
+              "flex items-center justify-center gap-x-1",
+            )}
+          >
+            <span className="hover:underline">GitHub</span>
+            <ArrowTopRightOnSquareIcon className="size-4 inline" />
+          </a>
+        </div>
+        Copyright © 2025 kitta65
+      </footer>
     </>
   );
 }
