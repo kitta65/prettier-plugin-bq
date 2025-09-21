6335df7b6f29418694c8f028aa46e1ad3af1d041
kitta65 <26474260+kitta65@users.noreply.github.com>
2025-09-21 13:06:03 +0900

    use tailwind

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1 className="font-extrabold text-5xl">Prettier Plugin BQ</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
